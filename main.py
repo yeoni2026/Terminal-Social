bc089d48eebbda7c363c4a20546aa2e0d9a5ee4b
yeoni2026 <yeoni2026@gmail.com>
2026-07-24 13:09:00 +0900

    main.py 작성

---
+++ main.py
@@ -0,0 +1,21 @@
+from user.py import User
+
+user = User()
+
+while True:
+    print("1. 메모하기  2. 종료")
+    user_input = int(input("> "))
+    print()
+
+    match(user_input):
+        case 1:
+            print("메모할 내용을 적어주세요.")
+            memo = input("> ")
+            user.save_memo(memo)
+            print("메모가 저장되었습니다!\n")
+            continue
+        case 2:
+            break
+        case _:
+            print("1~2 사이의 숫자를 입력해주세요.\n")
+            continue
